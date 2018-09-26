d3c61905f3bde6b625be535487eb297dfa4350cf
AlexandruCenusa <43582860+AlexandruCenusa@users.noreply.github.com>
2018-09-26 18:06:45 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Proiect</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+
+    <body>
+        <header>
+            
+        </header>
+
+        <nav>
+            <ul id="bara1">
+                <li id="buton"> <a href="acasa.php"> Acasa </a> </li>
+                <li id="buton"> <a href="produse.php"> Produse </a> </li>
+                <li id="buton"> <a href="despre.php"> Despre </a> </li>
+                <li id="buton"> <a href="contact.php"> Contact </a> </li>
+                <li id="buton" style="float: right; padding-right: 50px;"> <a href="register.php"> Register </a> </li>
+                <li id="buton" style="float: right;"> <a href="login.php"> Login </a> </li>
+                <li id="buton" style="float: right;"> <a href="profil.php"> Profil </a> </li>
+            </ul>
+        </nav>
+
+    </body>
+
+</html>
